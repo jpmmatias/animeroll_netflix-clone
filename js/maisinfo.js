150ab277df0a6b101195e4546903bdea934ec47f
J0ão Pedro <jpmmatias2018@outlook.com>
2021-03-22 19:14:22 -0300

    First commit

---
+++ js/maisinfo.js
@@ -0,0 +1,10 @@
+window.onload = ()=>{
+  const maisInfo = () =>{
+    alert(`Akame ga Kill! (アカメが斬る! Akame ga Kiru!?, o significado literal pode ser "Assassina de Olhos Vermelhos" ou até mesmo "A Assassina Akame") é uma série de mangá shonen Japonês escrita por Takahiro e ilustrada por Tetsuya Tashiro. Iniciou sua serialização na Gangan Joker da Square Enix em Março de 2010. A história se foca em Tatsumi, um jovem aldeão que viajou para a capital em busca de dinheiro para a sua vila e descobre uma forte corrupção naquela área. Uma assassina (Leone) do grupo Night Raid recruta o jovem garoto para ajudá-los na luta contra o Império para dar fim a corrupção. Juntos com o casal Rillary e Lubbock derrotando criaturas perigosas até um deles morrer.`)
+  }
+ const button =  document.querySelector("#info")
+ button.addEventListener('click',maisInfo)
+
+
+}
+
